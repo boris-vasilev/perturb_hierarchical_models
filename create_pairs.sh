a44adbeb9a17cdfc366cfda34e4aaa9434bd2544
Boris Vasilev <borisvasilev395@gmail.com>
2025-09-30 12:35:57 +0100

    Add unfiltered to create_pairs.sh

--- create_pairs.sh
+++ create_pairs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-#Rscript create_perturbation_eqtl_pairs.R --cells Jurkat --efficient
-#Rscript create_perturbation_eqtl_pairs.R --cells HepG2 --efficient
-Rscript create_perturbation_eqtl_pairs.R --cells K562_GenomeWide --efficient
-#Rscript create_perturbation_eqtl_pairs.R --cells RPE1 --efficient
+# Rscript create_perturbation_eqtl_pairs.R --cells Jurkat --efficient
+# Rscript create_perturbation_eqtl_pairs.R --cells HepG2 --efficient
+# Rscript create_perturbation_eqtl_pairs.R --cells K562_GenomeWide --efficient
+# Rscript create_perturbation_eqtl_pairs.R --cells K562_essential --efficient
+# Rscript create_perturbation_eqtl_pairs.R --cells RPE1 --efficient
+
+# Create unfiltered (any FDR) set of perturbation pairs
+Rscript create_perturbation_eqtl_pairs.R --cells Jurkat --efficient --unfiltered
+Rscript create_perturbation_eqtl_pairs.R --cells HepG2 --efficient --unfiltered
+Rscript create_perturbation_eqtl_pairs.R --cells K562_GenomeWide --efficient --unfiltered
+Rscript create_perturbation_eqtl_pairs.R --cells K562_essential --efficient --unfiltered
+Rscript create_perturbation_eqtl_pairs.R --cells RPE1 --efficient --unfiltered
