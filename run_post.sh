d7c59a0603f2c4201826e0b303d6e52341c3a719
Boris Vasilev <borisvasilev395@gmail.com>
2025-10-21 08:17:00 +0100

    Add cell lines to run_post

--- run_post.sh
+++ run_post.sh
@@ -1,38 +1,39 @@
 # K562_essential
-# Rscript post_gaussian.R --cells K562_essential --response ratio --model varying_intercept_fixed_slope --efficient &
-# Rscript post_gaussian.R --cells K562_essential --response ratio --model varying_intercept_varying_slope --efficient &
-# Rscript post_gaussian.R --cells K562_essential --response ratio --model vivs_student --efficient &
 
 # Rscript post_concordance.R --cells K562_essential --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells K562_essential --response ratio --model vivs_concordance --efficient &
-# Rscript post_concordance.R --cells K562_essential --response ratio --model vivs_student_concordance --efficient &
 
 Rscript post_logistic.R --cells K562_essential --model varying_intercept_fixed_slope --efficient &
 Rscript post_logistic.R --cells K562_essential --model varying_intercept_varying_slope --efficient &
-Rscript post_logistic.R --cells K562_essential --model vivs_student --efficient &
 
 # K562_GenomeWide
-# Rscript post_gaussian.R --cells K562_GenomeWide --response ratio --model varying_intercept_fixed_slope --efficient &
-# Rscript post_gaussian.R --cells K562_GenomeWide --response ratio --model varying_intercept_varying_slope --efficient &
-# Rscript post_gaussian.R --cells K562_GenomeWide --response ratio --model vivs_student --efficient &
 
 # Rscript post_concordance.R --cells K562_GenomeWide --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells K562_GenomeWide --response ratio --model vivs_concordance --efficient &
-# Rscript post_concordance.R --cells K562_GenomeWide --response ratio --model vivs_student_concordance --efficient &
 
-Rscript post_logistic.R --cells K562_GenomeWide --model varying_intercept_fixed_slope --efficient &
-Rscript post_logistic.R --cells K562_GenomeWide --model varying_intercept_varying_slope --efficient &
-Rscript post_logistic.R --cells K562_GenomeWide --model vivs_student --efficient &
+# Rscript post_logistic.R --cells K562_GenomeWide --model varying_intercept_fixed_slope --efficient &
+# Rscript post_logistic.R --cells K562_GenomeWide --model varying_intercept_varying_slope --efficient &
 
 # Jurkat
-# Rscript post_gaussian.R --cells Jurkat --response ratio --model varying_intercept_fixed_slope --efficient &
-# Rscript post_gaussian.R --cells Jurkat --response ratio --model varying_intercept_varying_slope --efficient &
-# Rscript post_gaussian.R --cells Jurkat --response ratio --model vivs_student --efficient &
 
 # Rscript post_concordance.R --cells Jurkat --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells Jurkat --response ratio --model vivs_concordance --efficient &
-# Rscript post_concordance.R --cells Jurkat --response ratio --model vivs_student_concordance --efficient &
 
-# Rscript post_logistic.R --cells Jurkat --model varying_intercept_fixed_slope --efficient &
-# Rscript post_logistic.R --cells Jurkat --model varying_intercept_varying_slope --efficient &
-# Rscript post_logistic.R --cells Jurkat --model vivs_student --efficient &
+Rscript post_logistic.R --cells Jurkat --model varying_intercept_fixed_slope --efficient &
+Rscript post_logistic.R --cells Jurkat --model varying_intercept_varying_slope --efficient &
+
+# RPE1
+
+# Rscript post_concordance.R --cells RPE1 --response ratio --model vifs_concordance --efficient &
+# Rscript post_concordance.R --cells RPE1 --response ratio --model vivs_concordance --efficient &
+
+Rscript post_logistic.R --cells RPE1 --model varying_intercept_fixed_slope --efficient &
+Rscript post_logistic.R --cells RPE1 --model varying_intercept_varying_slope --efficient &
+
+# HepG2
+
+# Rscript post_concordance.R --cells HepG2 --response ratio --model vifs_concordance --efficient &
+# Rscript post_concordance.R --cells HepG2 --response ratio --model vivs_concordance --efficient &
+
+Rscript post_logistic.R --cells HepG2 --model varying_intercept_fixed_slope --efficient &
+Rscript post_logistic.R --cells HepG2 --model varying_intercept_varying_slope --efficient &
