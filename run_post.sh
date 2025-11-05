c85d27192fd31e26e0fbaaba5fc7b043cc31310e
Boris Vasilev <borisvasilev395@gmail.com>
2025-11-05 13:27:12 +0000

    Update run_post

--- run_post.sh
+++ run_post.sh
@@ -3,7 +3,7 @@
 # Rscript post_concordance.R --cells K562_essential --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells K562_essential --response ratio --model vivs_concordance --efficient &
 
-Rscript post_logistic.R --cells K562_essential --model varying_intercept_fixed_slope --efficient &
+# Rscript post_logistic.R --cells K562_essential --model varying_intercept_fixed_slope --efficient &
 Rscript post_logistic.R --cells K562_essential --model varying_intercept_varying_slope --efficient &
 
 # K562_GenomeWide
@@ -19,7 +19,7 @@ Rscript post_logistic.R --cells K562_essential --model varying_intercept_varying
 # Rscript post_concordance.R --cells Jurkat --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells Jurkat --response ratio --model vivs_concordance --efficient &
 
-Rscript post_logistic.R --cells Jurkat --model varying_intercept_fixed_slope --efficient &
+# Rscript post_logistic.R --cells Jurkat --model varying_intercept_fixed_slope --efficient &
 Rscript post_logistic.R --cells Jurkat --model varying_intercept_varying_slope --efficient &
 
 # RPE1
@@ -27,7 +27,7 @@ Rscript post_logistic.R --cells Jurkat --model varying_intercept_varying_slope -
 # Rscript post_concordance.R --cells RPE1 --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells RPE1 --response ratio --model vivs_concordance --efficient &
 
-Rscript post_logistic.R --cells RPE1 --model varying_intercept_fixed_slope --efficient &
+# Rscript post_logistic.R --cells RPE1 --model varying_intercept_fixed_slope --efficient &
 Rscript post_logistic.R --cells RPE1 --model varying_intercept_varying_slope --efficient &
 
 # HepG2
@@ -35,5 +35,5 @@ Rscript post_logistic.R --cells RPE1 --model varying_intercept_varying_slope --e
 # Rscript post_concordance.R --cells HepG2 --response ratio --model vifs_concordance --efficient &
 # Rscript post_concordance.R --cells HepG2 --response ratio --model vivs_concordance --efficient &
 
-Rscript post_logistic.R --cells HepG2 --model varying_intercept_fixed_slope --efficient &
+# Rscript post_logistic.R --cells HepG2 --model varying_intercept_fixed_slope --efficient &
 Rscript post_logistic.R --cells HepG2 --model varying_intercept_varying_slope --efficient &
