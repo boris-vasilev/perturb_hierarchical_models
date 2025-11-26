b4363e0472e378dd4f254442ad2907950edfecda
Boris Vasilev <biv22@cam.ac.uk>
2025-11-26 10:31:04 +0000

    Fix precompile

--- cross_screen_models/bin/precompile_models.R
+++ cross_screen_models/bin/precompile_models.R
@@ -10,9 +10,9 @@ mod.me_j <- cmdstan_model(file.path(models_dir, "single_gene_me_j.stan"))
 mod.me_both <- cmdstan_model(file.path(models_dir, "single_gene_full_me.stan"))
 
 # Explicitly save the executables in the same folder
-file.copy(mod.no_me$exe_file(),   file.path(models_dir, "single_gene_no_me"),   overwrite = TRUE)
-file.copy(mod.me_j$exe_file(),    file.path(models_dir, "single_gene_me_j"),    overwrite = TRUE)
-file.copy(mod.me_both$exe_file(), file.path(models_dir, "single_gene_full_me"), overwrite = TRUE)
+#file.copy(mod.no_me$exe_file(),   file.path(models_dir, "single_gene_no_me"),   overwrite = TRUE)
+#file.copy(mod.me_j$exe_file(),    file.path(models_dir, "single_gene_me_j"),    overwrite = TRUE)
+#file.copy(mod.me_both$exe_file(), file.path(models_dir, "single_gene_full_me"), overwrite = TRUE)
 
 # Write flag
 file.create("precompile_done.flag")
