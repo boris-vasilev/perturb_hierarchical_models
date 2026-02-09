64f31b0bf6a6ec0abfd95478377e8df8875fc0d7
Boris Vasilev <borisvasilev395@gmail.com>
2026-02-09 17:48:03 +0000

    Missed shortened vivs in tsubmit_logistic

--- submit_logistic.sh
+++ submit_logistic.sh
@@ -1,19 +1,19 @@
 # Jurkat
-sbatch --job-name="l-J-vivs" logistic.sh --cells Jurkat --model varying_intercept_varying_slope
+sbatch --job-name="l-J-vivs" logistic.sh --cells Jurkat --model vivs
 sbatch --job-name="l-J-vivs_hs" logistic.sh --cells Jurkat --model vivs_horseshoe
 
 # K562_essential
-sbatch --job-name="l-KE-vivs" logistic.sh --cells K562_essential --model varying_intercept_varying_slope
+sbatch --job-name="l-KE-vivs" logistic.sh --cells K562_essential --model vivs
 sbatch --job-name="l-KE-vivs_hs" logistic.sh --cells K562_essential --model vivs_horseshoe
 
 # K562_GenomeWide
-# sbatch --job-name="l-KG-vivs" logistic.sh --cells K562_GenomeWide --model varying_intercept_varying_slope
+# sbatch --job-name="l-KG-vivs" logistic.sh --cells K562_GenomeWide --model vivs
 # sbatch --job-name="l-KG-vivs_hs" logistic.sh --cells K562_GenomeWide --model vivs_horseshoe
 
 # HepG2
-sbatch --job-name="l-H-vivs" logistic.sh --cells HepG2 --model varying_intercept_varying_slope
+sbatch --job-name="l-H-vivs" logistic.sh --cells HepG2 --model vivs
 sbatch --job-name="l-H-vivs_hs" logistic.sh --cells HepG2 --model vivs_horseshoe
 
 # RPE1
-sbatch --job-name="l-R-vivs" logistic.sh --cells RPE1 --model varying_intercept_varying_slope
+sbatch --job-name="l-R-vivs" logistic.sh --cells RPE1 --model vivs
 sbatch --job-name="l-R-vivs_hs" logistic.sh --cells RPE1 --model vivs_horseshoe
